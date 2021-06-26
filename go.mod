403029551e75900b2ace4b104c2c7decfaf57569
Gabriel Tiso <gabrieltisovinhasdebrito2@gmail.com>
2021-06-26 17:37:26 -0300

    feat: got rid of some code duplication, added support to POST, DELETE and PUT methods

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Gabriel2233/hai
 go 1.16
 
 require (
-	github.com/jroimartin/gocui v0.4.0 // indirect
+	github.com/jroimartin/gocui v0.4.0
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/nsf/termbox-go v1.1.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -15,10 +14,10 @@ import (
 )
 
 var (
-	methods      = []string{"GET", "POST", "DELETE", "PATCH"}
-	views        = []string{"input-bar", "method", "req-body", "res-output"}
-	activeView   = len(views) - 1
-	activeMethod = 0
+	methods       = []string{"GET", "POST", "DELETE", "PUT"}
+	views         = []string{"input-bar", "method", "history", "req-body", "res-output"}
+	active_view   = len(views) - 1
+	active_method = 0
 )
 
 type MethodWidget struct {
@@ -32,13 +31,13 @@ func NewMethodWidget(name, title string, x, y, w, h int) *MethodWidget {
 	return &MethodWidget{name: name, x: x, y: y, w: w, h: h}
 }
 
-func (iw *MethodWidget) Layout(g *ui.Gui) error {
-	v, err := g.SetView(iw.name, iw.x, iw.y, iw.x+iw.w, iw.y+iw.h)
+func (w *MethodWidget) Layout(g *ui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+w.h)
 	if err != nil {
 		if err != ui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintf(v, "%s", methods[activeMethod])
+		fmt.Fprintf(v, "%s", methods[active_method])
 	}
 	return nil
 }
@@ -50,12 +49,12 @@ type InputWidget struct {
 	w, h  int
 }
 
-func NewInputWidget(name, title string, x, y, w, h int, handlers ...func(g *ui.Gui, v *ui.View) error) *InputWidget {
+func NewInputWidget(name, title string, x, y, w, h int) *InputWidget {
 	return &InputWidget{name: name, title: title, x: x, y: y, w: w, h: h}
 }
 
-func (iw *InputWidget) Layout(g *ui.Gui) error {
-	v, err := g.SetView(iw.name, iw.x, iw.y, iw.x+iw.w, iw.y+iw.h)
+func (w *InputWidget) Layout(g *ui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+w.h)
 	if err != nil {
 		if err != ui.ErrUnknownView {
 			return err
@@ -65,7 +64,33 @@ func (iw *InputWidget) Layout(g *ui.Gui) error {
 		}
 
 	}
-	v.Title = iw.title
+	v.Title = w.title
+	v.Editable = true
+	return nil
+}
+
+type HelperMenuWidget struct {
+	name  string
+	title string
+	x, y  int
+	w, h  int
+}
+
+func NewHelperMenuWidget(name, title string, x, y, w, h int) *HelperMenuWidget {
+	return &HelperMenuWidget{name: name, title: title, x: x, y: y, w: w, h: h}
+}
+
+func (w *HelperMenuWidget) Layout(g *ui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+w.h)
+	if err != nil {
+		if err != ui.ErrUnknownView {
+			return err
+		}
+		if _, err := g.SetCurrentView(v.Name()); err != nil {
+			log.Panicln(err)
+		}
+	}
+	v.Title = w.title
 	v.Editable = true
 	return nil
 }
@@ -81,14 +106,14 @@ func NewRequestBodyWidget(name, title string, x, y, w, h int) *RequestBodyWidget
 	return &RequestBodyWidget{name: name, title: title, x: x, y: y, w: w, h: h}
 }
 
-func (iw *RequestBodyWidget) Layout(g *ui.Gui) error {
-	v, err := g.SetView(iw.name, iw.x, iw.y, iw.x+iw.w, iw.y+iw.h)
+func (w *RequestBodyWidget) Layout(g *ui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+w.h)
 	if err != nil {
 		if err != ui.ErrUnknownView {
 			return err
 		}
 	}
-	v.Title = iw.title
+	v.Title = w.title
 	v.Editable = true
 	v.Wrap = true
 	return nil
@@ -105,14 +130,14 @@ func NewResultsWidget(name, title string, x, y, w, h int) *ResultsWidget {
 	return &ResultsWidget{name: name, title: title, x: x, y: y, w: w, h: h}
 }
 
-func (iw *ResultsWidget) Layout(g *ui.Gui) error {
-	v, err := g.SetView(iw.name, iw.x, iw.y, iw.x+iw.w, iw.y+iw.h)
+func (w *ResultsWidget) Layout(g *ui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+w.h)
 	if err != nil {
 		if err != ui.ErrUnknownView {
 			return err
 		}
 	}
-	v.Title = iw.title
+	v.Title = w.title
 	return nil
 }
 
@@ -124,16 +149,17 @@ func main() {
 	defer g.Close()
 
 	g.Highlight = true
-	g.SelFgColor = ui.ColorRed
+	g.SelFgColor = ui.ColorGreen
 
-	maxX, maxY := g.Size()
+	max_x, max_y := g.Size()
 
-	input := NewInputWidget("input-bar", "Api URL", 0, 0, maxX-12, 2)
-	method := NewMethodWidget("method", methods[activeMethod], maxX-11, 0, maxX-2-(maxX-11), 2)
-	res_body := NewRequestBodyWidget("req-body", "Response Body", 0, 3, maxX/3, maxY-4)
-	res_output := NewResultsWidget("res-output", "Results", maxX/3+1, 3, maxX-2-(maxX/3), maxY-4)
+	input := NewInputWidget("input-bar", "Api URL", 0, 0, max_x-12, 2)
+	method := NewMethodWidget("method", methods[active_method], max_x-11, 0, max_x-2-(max_x-11), 2)
+	history := NewHelperMenuWidget("history", "History", 0, 3, max_x/3, max_y/3)
+	res_body := NewRequestBodyWidget("req-body", "Response Body", 0, max_y/3+4, max_x/3, max_y-2-(max_y/2-3))
+	res_output := NewResultsWidget("res-output", "Results", max_x/3+1, 3, max_x-2-(max_x/3), max_y-4)
 
-	g.SetManager(input, method, res_body, res_output)
+	g.SetManager(input, method, history, res_body, res_output)
 	if err := initBindings(g); err != nil {
 		log.Panicln(err)
 	}
@@ -150,43 +176,78 @@ func initBindings(g *ui.Gui) error {
 	if err := g.SetKeybinding("", ui.KeyTab, ui.ModNone, nextView); err != nil {
 		return err
 	}
+	if err := g.SetKeybinding("", ui.KeyCtrlS, ui.ModNone, processRequest); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", ui.KeyCtrlL, ui.ModNone, clear); err != nil {
+		return err
+	}
 	if err := g.SetKeybinding("method", ui.KeySpace, ui.ModNone, nextMethod); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding("", ui.KeyCtrlS, ui.ModNone, processRequest); err != nil {
+	if err := g.SetKeybinding("res-output", ui.KeyArrowUp, ui.ModNone, cursorUp); err != nil {
 		return err
+	}
+	if err := g.SetKeybinding("res-output", ui.KeyArrowDown, ui.ModNone, cursorDown); err != nil {
+		return err
+	}
+	return nil
+}
+
+func cursorDown(g *ui.Gui, v *ui.View) error {
+	if v != nil {
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy+1); err != nil {
+			ox, oy := v.Origin()
+			if err := v.SetOrigin(ox, oy+1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func cursorUp(g *ui.Gui, v *ui.View) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+			if err := v.SetOrigin(ox, oy-1); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
 
 func nextView(g *ui.Gui, v *ui.View) error {
-	nextIndex := (activeView + 1) % len(views)
-	name := views[nextIndex]
+	next_index := (active_view + 1) % len(views)
+	name := views[next_index]
 
 	if _, err := g.SetCurrentView(name); err != nil {
 		return err
 	}
 
-	if nextIndex == 0 || nextIndex == 2 {
+	if next_index != 1 {
 		g.Cursor = true
 	} else {
 		g.Cursor = false
 	}
 
-	activeView = nextIndex
+	active_view = next_index
 	return nil
 }
 
 func nextMethod(g *ui.Gui, v *ui.View) error {
-	nextIndex := (activeMethod + 1) % len(views)
-	label := methods[nextIndex]
+	next_index := (active_method + 1) % len(methods)
+	label := methods[next_index]
 
 	v.Clear()
 	if _, err := v.Write([]byte(label)); err != nil {
 		return err
 	}
 
-	activeMethod = nextIndex
+	active_method = next_index
 	return nil
 }
 
@@ -196,7 +257,6 @@ func processRequest(g *ui.Gui, v *ui.View) error {
 		return err
 	}
 	i_buf := input_view.ViewBufferLines()
-
 	if len(i_buf) <= 0 {
 		return nil
 	}
@@ -205,8 +265,7 @@ func processRequest(g *ui.Gui, v *ui.View) error {
 
 	_, err = url.ParseRequestURI(api_url)
 	if err != nil {
-		input_view.Clear()
-		input_view.SetCursor(0, 0)
+		return nil
 	}
 
 	request_view, err := g.View("req-body")
@@ -215,63 +274,165 @@ func processRequest(g *ui.Gui, v *ui.View) error {
 	}
 	req_buf := request_view.Buffer()
 
-	if req_buf == "" && activeMethod == 1 || activeMethod == 3 {
-		return errors.New("cannot do this request without a body")
-	}
-
-	if activeMethod == 1 || activeMethod == 3 {
-		var data map[string]interface{}
-		if err := json.Unmarshal([]byte(req_buf), &data); err != nil {
-			request_view.Clear()
-			request_view.SetCursor(0, 0)
-			return err
+	var request_body_data []byte
+	if active_method == 1 || active_method == 3 {
+		valid := json.Valid([]byte(req_buf))
+		if !valid {
+			return nil
 		}
+		request_body_data = []byte(req_buf)
 	}
 
-	switch activeMethod {
+	resultsChan := make(chan string)
+
+	switch active_method {
 	case 0:
-		start := time.Now()
-		res, err := http.Get(api_url)
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close()
-
-		elapsed := time.Since(start).Seconds()
-
 		out, err := g.View("res-output")
 		if err != nil {
 			return err
 		}
-
-		fmt.Fprintln(out, "\n-------- HEADERS")
-		for name, headers := range res.Header {
-			for _, h := range headers {
-				fmt.Fprintf(out, "%v: %v\n", name, h)
-			}
-		}
-
-		fmt.Fprintln(out, "\n--------")
-
-		body, err := io.ReadAll(res.Body)
+		go httpRequest(api_url, "GET", nil, out, resultsChan)
+		fmt.Fprintln(out, <-resultsChan)
+	case 1:
+		out, err := g.View("res-output")
 		if err != nil {
 			return err
 		}
-
-		var dst bytes.Buffer
-		err = json.Indent(&dst, body, "", "    ")
+		go httpRequest(api_url, "POST", request_body_data, out, resultsChan)
+		fmt.Fprintln(out, <-resultsChan)
+	case 2:
+		out, err := g.View("res-output")
 		if err != nil {
 			return err
 		}
-
-		fmt.Fprintf(out, "Body: \n%s\n", dst.String())
-		fmt.Fprintf(out, "Status: %d\n", res.StatusCode)
-		fmt.Fprintf(out, "Time: %f s\n", elapsed)
+		go httpRequest(api_url, "DELETE", request_body_data, out, resultsChan)
+		fmt.Fprintln(out, <-resultsChan)
+	case 3:
+		out, err := g.View("res-output")
+		if err != nil {
+			return err
+		}
+		go httpRequest(api_url, "PUT", request_body_data, out, resultsChan)
+		fmt.Fprintln(out, <-resultsChan)
 	}
 
 	return nil
 }
 
+func httpRequest(url, method string, body []byte, v *ui.View, ch chan string) error {
+	start := time.Now()
+	duration := time.Since(start).Seconds()
+	fmt.Fprintf(v, "Time: %f s\n", duration)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	req.Header.Add("content-type", "application/json")
+	if err != nil {
+		ch <- fmt.Sprintf("Error: %s\n", err.Error())
+		return nil
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		ch <- fmt.Sprintf("Error: %s\n", err.Error())
+		return nil
+	}
+
+	result, err := formatResponse(res)
+	if err != nil {
+		ch <- fmt.Sprintf("Error: %s\n", err.Error())
+		return nil
+	}
+	defer res.Body.Close()
+
+	ch <- result
+
+	return nil
+}
+
+type Result struct {
+	method string
+	url    string
+	path   string
+	proto  string
+	header http.Header
+	status string
+	body   string
+}
+
+func formatResponse(res *http.Response) (string, error) {
+	var resultStr string
+
+	r := Result{
+		method: res.Request.Method,
+		url:    res.Request.URL.String(),
+		path:   res.Request.URL.Path,
+		proto:  res.Proto,
+		status: res.Status,
+		header: res.Header,
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	r.body = string(body)
+
+	resultStr += fmt.Sprintf("%s %s %s\n", r.method, r.path, r.proto)
+	resultStr += fmt.Sprintf("Status %s \n", r.status)
+	resultStr += "\n"
+
+	for name, headers := range r.header {
+		for _, h := range headers {
+			resultStr += fmt.Sprintf("%s => %s\n", name, h)
+		}
+	}
+
+	resultStr += "\n"
+	resultStr += fmt.Sprintf("body  \n %s", r.body)
+
+	history = append(history, resultStr)
+
+	return resultStr, nil
+}
+
+// func saveRequest(result Result) error {
+// 	content := fmt.Sprintf("%s %s/%s %s\n", result.method, result.url, result.path, result.status)
+
+// 	home, err := homedir.Dir()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	file_path := fmt.Sprintf("%s/hai.txt", home)
+
+// 	f, err := os.Create(file_path)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	w := bufio.NewWriter(f)
+// 	_, err = w.WriteString(content)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
 func quit(g *ui.Gui, v *ui.View) error {
 	return ui.ErrQuit
+}
+
+func clear(g *ui.Gui, v *ui.View) error {
+	return clearView(v)
+}
+
+func clearView(v *ui.View) error {
+	v.Clear()
+	if err := v.SetCursor(0, 0); err != nil {
+		return err
+	}
+	return nil
 }
